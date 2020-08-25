7ff6f8036dc02441ae19e921f41c24fc9701400b
Beata Tomczak <tma.btty@gmail.com>
2020-08-25 16:41:45 +0200

    Create the bricks

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const rulesBtn = document.getElementById('rules-btn');
-const closeBtn = document.getElementById('close-btn');
-const rules = document.getElementById('rules');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const rulesBtn = document.getElementById('rules-btn')
+const closeBtn = document.getElementById('close-btn')
+const rules = document.getElementById('rules')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
-let score = 0;
+let score = 0
+
+const brickRowCount = 9
+const brickColumnCount = 5
 
 // Create ball props
 const ball = {
@@ -24,42 +27,77 @@ const paddle = {
     h: 10,
     speed: 8,
     dx: 0
+} 
+
+//Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+// Create the brick
+const bricks = []
+for(let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
 }
 
 // Draw ball on canvas
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-    ctx.fillStyle = '#e0105c';
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#e0105c'
+    ctx.fill()
+    ctx.closePath()
 }
 
 // Draw paddle on canvas
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-    ctx.fillStyle = '#e0105c';
-    ctx.fill();
-    ctx.closePath();
-}
-
-// Draw everything
-function draw() {
-    drawBall();
-    drawPaddle();
-    drawScore();
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#474747'
+    ctx.fill()
+    ctx.closePath()
 }
 
 // Draw score on canvas
 function drawScore() {
-    ctx.font = '20px Arial';
-    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+    ctx.font = '22px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
 }
 
-draw();
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? '#474747' : 'transparent'
+            ctx.fill()
+            ctx.closePath()
+        }) 
+    })
+}
+
+// Draw everything
+function draw() {
+    drawBall()
+    drawPaddle()
+    drawScore()
+    drawBricks()
+}
+
+draw()
 
 
 // Rules and close event handlers
-rulesBtn.addEventListener('click', () => rules.classList.add('show'));
-closeBtn.addEventListener('click', () => rules.classList.remove('show'));
+rulesBtn.addEventListener('click', () => rules.classList.add('show'))
+closeBtn.addEventListener('click', () => rules.classList.remove('show'))
